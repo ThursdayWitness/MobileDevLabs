ab7c4ef07c2a139a11bdca2afac2746c24354199
ThursdayWitness <lirkir1256@gmail.com>
2023-03-03 16:35:59 +0500

    Small format fixes

--- lib/lab1.dart
+++ lib/lab1.dart
@@ -1,37 +1,36 @@
 import 'package:flutter/material.dart';
 
-
 void main() => runApp(const MainPage());
 
-class MainPage extends StatefulWidget
-{
+class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage>
-{
+class _MainPageState extends State<MainPage> {
   final myController = TextEditingController();
   var isTextVisible = false;
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(),
+            appBar: AppBar(title: const Text("Lab1: «Hello World»")),
             body: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 TextField(controller: myController),
-                ElevatedButton(onPressed: () {isTextVisible = true; setState((){});}, child: const Text("Отобразить")),
-                if(isTextVisible) Text(myController.text),
+                ElevatedButton(
+                    onPressed: () {
+                      isTextVisible = !isTextVisible;
+                      setState(() {});
+                    },
+                    child: const Text("Отобразить")),
+                if (isTextVisible) Text(myController.text),
               ],
-            )
-        )
-    );
+            )));
   }
 }
